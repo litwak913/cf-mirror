cbdeeadf6f1799372ec13a052c319b887fd189b9
litwak913 <litwak913@qq.com>
2020-06-27 17:03:51 +0800

    super bug fix

--- src/main/java/io/github/litwak913/CFMirror.java
+++ src/main/java/io/github/litwak913/CFMirror.java
@@ -1,45 +1,65 @@
 package io.github.litwak913;
 
+import com.google.gson.Gson;
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
+import io.github.litwak913.gson.Mods;
+
 import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CFMirror {
 
     private String jsonString;
 
-    public void startMirror(String[] mirror,String mode,boolean verbose,String dir){
-        if(mirror[0]=="mods"){
+    public void startMirror(String[] mirror, String mode, boolean verbose, String dir) {
+        System.out.println("Start mirror ");
+        if (mirror[0].equals("mods")) {
             mirrorMods();
-            if (mirror.length==2) {
-                if (mirror[1] == "modpacks") {
+            if (mirror.length == 2) {
+                if (mirror[1].equals("modpacks")) {
                     mirrorModpacks();
                 }
             }
-        } else if (mirror[0]=="modpacks"){
+        }
+        if (mirror[0].equals("modpacks")) {
             mirrorModpacks();
-            if (mirror.length==2) {
-                if (mirror[1] == "mods") {
+            if (mirror.length == 2) {
+                if (mirror[1].equals("mods")) {
                     mirrorMods();
                 }
             }
         }
     }
 
-    private void mirrorModpacks()  {
+    private void mirrorModpacks() {
 
     }
 
     private void mirrorMods() {
-        for(int i=0;i<100;i++)
-            try{
-                jsonString = Http.doHttpRequest("api/v2/addon/search?gameId=432&index=" + i * 100 + "&pageSize=100&sort=1&sectionId=6");
-            } catch (IOException e){
-                System.out.println("\033[31;4m"+"ERROR:"+e.getMessage()+"\033[0m");
+        System.out.println("Start mirror mods");
+        for (int i = 0; i < 100; i++)
+            try {
+                jsonString = Http.doHttpRequest("api/v2/addon/search?gameId=432&index=" + 1 * i + "&pageSize=3&sort=1&sectionId=6");
+                if (jsonString .equals( "ERROR")) {
+                    System.out.println("\033[31;4m" + "ERROR:Network Error" + "\033[0m");
+                    System.exit(1);
+                }
+                System.out.println(jsonString);
+                Gson gson = new Gson();
+                Type type = new TypeToken<ArrayList<Mods>>() {
+                }.getType();
+                List<Mods> list = gson.fromJson(jsonString, type);
+                for (Mods v : list) {
+                    System.out.println(v.getId());
+                }
+            } catch (IOException e) {
+                System.out.println("\033[31;4m" + "ERROR:" + e.getMessage() + "\033[0m");
                 System.exit(1);
             }
-        if(jsonString=="ERROR"){
-            System.out.println("\033[31;4m"+"ERROR:Network Error"+"\033[0m");
-            System.exit(1);
-        }
-        System.out.println(jsonString);
+
+
     }
 }
--- src/main/java/io/github/litwak913/Main.java
+++ src/main/java/io/github/litwak913/Main.java
@@ -6,36 +6,37 @@ import org.apache.commons.cli.*;
 public class Main {
     private static CommandLine commandLine;
 
-    public static void main(String[] args)  {
-        CommandLineParser parser=new BasicParser();
+    public static void main(String[] args) {
+        System.out.println("cf-copy 1.0.0");
+        CommandLineParser parser = new GnuParser();
 
-        Options options=new Options();
-        options.addOption("t","type",true,"set mirror type");
-        options.addOption("m","mode",true,"set mirror mode");
-        options.addOption("o","output",false,"set mirror output dir");
-        options.addOption("v","verbose",false,"verbose mode");
+        Options options = new Options();
+        options.addOption("t", "type", true, "set mirror type");
+        options.addOption("m", "mode", true, "set mirror mode");
+        options.addOption("o", "output", true, "set mirror output dir");
+        options.addOption("v", "verbose", false, "verbose mode");
         try {
-             commandLine = parser.parse(options, args);
-        } catch(ParseException e ){
-            System.out.println("\033[31;4m"+"ERROR:Parse Error"+e.toString()+"\033[0m");
+            commandLine = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.out.println("\033[31;4m" + "ERROR:Parse Error" + e.toString() + "\033[0m");
             System.exit(1);
         }
-        boolean verbose=false;
-        String outputdir="";
-        String modes="";
+        boolean verbose = false;
+        String outputdir = "";
+        String modes = "";
         String[] type = {};
-        if (commandLine.hasOption("t")){
-            type=commandLine.getOptionValue("t").split(" ");
+        if (commandLine.hasOption("t")) {
+            type = commandLine.getOptionValue("t").split(" ");
         }
-        if (commandLine.hasOption("m")){
-            modes=commandLine.getOptionValue("m");
+        if (commandLine.hasOption("m")) {
+            modes = commandLine.getOptionValue("m");
         }
-        if (commandLine.hasOption("o")){
-            outputdir=commandLine.getOptionValue("f");
+        if (commandLine.hasOption("o")) {
+            outputdir = commandLine.getOptionValue("f");
         }
-        if (commandLine.hasOption("v")){
-            verbose=true;
+        if (commandLine.hasOption("v")) {
+            verbose = true;
         }
-        new CFMirror().startMirror(type,modes,verbose,outputdir);
+        new CFMirror().startMirror(type, modes, verbose, outputdir);
     }
 }
