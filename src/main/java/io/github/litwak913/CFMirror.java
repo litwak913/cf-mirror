13de9002bfa162df4a504c2b75e831a812c9d747
litwak913 <litwak913@qq.com>
2020-07-05 14:12:12 +0800

    delete commons-cli,args encode,mirror modes

--- src/main/java/io/github/litwak913/CFMirror.java
+++ src/main/java/io/github/litwak913/CFMirror.java
@@ -1,12 +1,9 @@
 package io.github.litwak913;
 
 import com.google.gson.Gson;
-
 import com.google.gson.reflect.TypeToken;
 
 import java.io.IOException;
-import java.lang.reflect.Type;
-import java.util.ArrayList;
 import java.util.List;
 
 public class CFMirror {
@@ -14,29 +11,14 @@ public class CFMirror {
     private String jsonString;
     private String fileJsonString;
 
-    public void startMirror(String[] mirror, String mode, boolean verbose, String dir) {
+    public void startMirror(String mode, boolean verbose, String dir) {
 
-        if (mirror[0].equals("mods")) {
-            mirrorMods();
-            if (mirror.length == 2) {
-                if (mirror[1].equals("modpacks")) {
-                    mirrorModpacks();
-                }
-            }
-        }
-        if (mirror[0].equals("modpacks")) {
-            mirrorModpacks();
-            if (mirror.length == 2) {
-                if (mirror[1].equals("mods")) {
-                    mirrorMods();
-                }
-            }
-        }
+
+        mirrorMods();
     }
 
-    private void mirrorModpacks() {
 
-    }
+
 
     private void mirrorMods() {
         System.out.println("Start mirror mods");
@@ -48,21 +30,19 @@ public class CFMirror {
                     System.exit(1);
                 }
                 System.out.println(jsonString);
-                Gson gson = new Gson();
-                Type type = new TypeToken<ArrayList<Mods>>() {
-                }.getType();
-                List<Mods> list = gson.fromJson(jsonString, type);
+                List<Mods> list = new Gson().fromJson(jsonString, new TypeToken<List<Mods>>() {
+                }.getType());
                 for (Mods v : list) {
                     System.out.println(v.getId());
                     System.out.println("Get file list");
-                    fileJsonString=Http.doHttpRequest("api/v2/addon/"+v.getId()+"/files");
-                    Type type2=new  TypeToken<ArrayList<ModsFiles>>(){}.getType();
-                    Gson gson2 = new Gson();
-                      List<ModsFiles> mflist= gson.fromJson(fileJsonString, type2);
-                      for(ModsFiles mf:mflist) {
-                          System.out.println(mf.getFileName());
-                          System.out.println(mf.getDownloadUrl());
-                      }
+                    fileJsonString = Http.doHttpRequest("api/v2/addon/" + v.getId() + "/files");
+
+                    List<ModsFiles> mflist = new Gson().fromJson(fileJsonString, new TypeToken<List<ModsFiles>>() {
+                    }.getType());
+                    for (ModsFiles mf : mflist) {
+                        System.out.println(mf.getFileName());
+                        System.out.println(mf.getDownloadUrl());
+                    }
                 }
             } catch (IOException e) {
                 System.out.println("\033[31;4m" + "ERROR:" + e.getMessage() + "\033[0m");
--- src/main/java/io/github/litwak913/Main.java
+++ src/main/java/io/github/litwak913/Main.java
@@ -1,42 +1,21 @@
 package io.github.litwak913;
 
 
-import org.apache.commons.cli.*;
-
 public class Main {
-    private static CommandLine commandLine;
+
 
     public static void main(String[] args) {
-        System.out.println("cf-copy 1.0.0");
-        CommandLineParser parser = new GnuParser();
-
-        Options options = new Options();
-        options.addOption("t", "type", true, "set mirror type");
-        options.addOption("m", "mode", true, "set mirror mode");
-        options.addOption("o", "output", true, "set mirror output dir");
-        options.addOption("v", "verbose", false, "verbose mode");
-        try {
-            commandLine = parser.parse(options, args);
-        } catch (ParseException e) {
-            System.out.println("\033[31;4m" + "ERROR:Parse Error" + e.toString() + "\033[0m");
-            System.exit(1);
-        }
         boolean verbose = false;
-        String outputdir = "";
-        String modes = "";
-        String[] type = {};
-        if (commandLine.hasOption("t")) {
-            type = commandLine.getOptionValue("t").split(" ");
+        String dir = "./output";
+        if (args.length != 1) {
+            System.out.println("Usage: java -jar cf-mirror.jar <mode> [verbose] [dir] ");
+            System.out.println("Mode: \n list: mirror all mods lists. " +
+                    "\n file: mirror all mods files. " +
+                    "\n all: mirror all mods list and files");
         }
-        if (commandLine.hasOption("m")) {
-            modes = commandLine.getOptionValue("m");
+        if (args.length == 3) {
+            dir = args[2];
         }
-        if (commandLine.hasOption("o")) {
-            outputdir = commandLine.getOptionValue("f");
-        }
-        if (commandLine.hasOption("v")) {
-            verbose = true;
-        }
-        new CFMirror().startMirror(type, modes, verbose, outputdir);
+        new CFMirror().startMirror(args[0], verbose, dir);
     }
 }
