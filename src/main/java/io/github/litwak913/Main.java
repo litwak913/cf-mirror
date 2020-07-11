9e12464854fbde91442d03cb5901f0543f234ff8
litwak913 <litwak913@qq.com>
2020-07-11 23:20:41 +0800

    add version message

--- src/main/java/io/github/litwak913/Main.java
+++ src/main/java/io/github/litwak913/Main.java
@@ -8,9 +8,19 @@ public class Main {
 
     public static void main(String[] args) {
         Config cfg = new Config();
+        String jarpath = System.getProperty("java.class.path");
+
+        String[] jarpatharray = jarpath.split("/");
+        String jarName = jarpatharray[jarpatharray.length - 1];
         JCommander jcmd = new JCommander().newBuilder().addObject(cfg).build();
         if (cfg.help) {
-            jcmd.setProgramName("java -jar cf-mirror.jar");
+            jcmd.setProgramName("java -jar " + jarName);
+            jcmd.usage();
+            return;
+        }
+        if (cfg.version) {
+            JCommander.getConsole().println("CFMirror v1.0");
+            JCommander.getConsole().println("This program may be freely redistributed under the terms of the GNU General Public License.");
         }
         new CFMirror().startMirror(cfg.mode, cfg.dir);
     }
