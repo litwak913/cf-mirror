2aa5e67321f30cd7b6af7f05b73fdcfaff9b2322
litwak913 <litwak913@qq.com>
2020-07-11 15:26:23 +0800

    use commons-log,update http

--- src/main/java/io/github/litwak913/CFMirror.java
+++ src/main/java/io/github/litwak913/CFMirror.java
@@ -2,53 +2,42 @@ package io.github.litwak913;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.config.Configurator;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
-import java.io.IOException;
 import java.util.List;
 
 
 
 public class CFMirror {
-    private static final Logger logger = LogManager.getLogger(LogManager.ROOT_LOGGER_NAME);
+    private Log log;
     public void startMirror(String mode, boolean verbose, String dir) {
-        if (verbose) {
-            Configurator.setRootLevel(Level.ALL);
-        }
+        log = LogFactory.getLog(CFMirror.class);
+        log.info("Start mirror");
         mirrorMods();
     }
 
     private void mirrorMods() {
         System.out.println("Start mirror mods");
-        for (int i = 0; i < 100; i++)
-            try {
-                String jsonString = Http.doHttpRequest("api/v2/addon/search?gameId=432&index=" + 100 * i + "&pageSize=100&sort=1&sectionId=6");
-                if (jsonString.equals("ERROR")) {
-                    System.out.println("\033[31;4m" + "ERROR:Network Error" + "\033[0m");
-                    System.exit(1);
-                }
-                System.out.println(jsonString);
-                List<Mods> list = new Gson().fromJson(jsonString, new TypeToken<List<Mods>>() {
-                }.getType());
-                for (Mods v : list) {
-                    System.out.println(v.getId());
-                    System.out.println("Get file list");
-                    String fileJsonString = Http.doHttpRequest("api/v2/addon/" + v.getId() + "/files");
+        for (int i = 0; i < 100; i++) {
+            log.info("Downloading Mods index");
+            String jsonString = Http.doHttpRequest("api/v2/addon/search?gameId=432&index=" + 100 * i + "&pageSize=100&sort=1&sectionId=6");
+            log.debug(jsonString);
+            List<Mods> list = new Gson().fromJson(jsonString, new TypeToken<List<Mods>>() {
+            }.getType());
+            for (Mods v : list) {
+                log.debug(v.getId());
+                log.info("Get mod file list:" + v.getName());
+                String fileJsonString = Http.doHttpRequest("api/v2/addon/" + v.getId() + "/files");
 
-                    List<ModsFiles> mflist = new Gson().fromJson(fileJsonString, new TypeToken<List<ModsFiles>>() {
-                    }.getType());
-                    for (ModsFiles mf : mflist) {
-                        System.out.println(mf.getFileName());
-                        System.out.println(mf.getDownloadUrl());
-                    }
+                List<ModsFiles> mflist = new Gson().fromJson(fileJsonString, new TypeToken<List<ModsFiles>>() {
+                }.getType());
+                for (ModsFiles mf : mflist) {
+                    log.debug(mf.getFileName());
+                    log.debug(mf.getDownloadUrl());
                 }
-            } catch (IOException e) {
-                System.out.println("\033[31;4m" + "ERROR:" + e.getMessage() + "\033[0m");
-                System.exit(1);
             }
+        }
 
 
     }
--- src/main/java/io/github/litwak913/Http.java
+++ src/main/java/io/github/litwak913/Http.java
@@ -1,26 +1,60 @@
 package io.github.litwak913;
 
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 
 public class Http {
     public static final String CF_URL = "https://addons-ecs.forgesvc.net/";
 
-    public static String doHttpRequest(String extra) throws IOException {
-        OkHttpClient http = new OkHttpClient.Builder()
-                .connectTimeout(30, TimeUnit.SECONDS)
-                .readTimeout(5, TimeUnit.MINUTES)
-                .build();
+    public static String doHttpRequest(String extra) {
+        Log log;
+        String url = CF_URL + extra;
+        int timeout = 60000;
+        CloseableHttpClient httpClient = null;
+        CloseableHttpResponse response = null;
+        String result = null;
+        log = LogFactory.getLog(Http.class);
+        try {
+            httpClient = HttpClients.createDefault();
+            HttpGet httpGet = new HttpGet(url);
+            RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(timeout)
+                    .setConnectionRequestTimeout(timeout)
+                    .setSocketTimeout(timeout)
+                    .build();
+            httpGet.setConfig(requestConfig);
+            response = httpClient.execute(httpGet);
 
-        Request request = new Request.Builder().get().url(CF_URL + extra).build();
-        Response response = http.newCall(request).execute();
-        if (response.isSuccessful()) {
-            return response.body().string();
+            HttpEntity entity = response.getEntity();
+
+            result = EntityUtils.toString(entity);
+        } catch (IOException e) {
+            log.error("Network Error", e);
+        } finally {
+            if (null != response) {
+                try {
+                    response.close();
+                } catch (IOException e) {
+                    log.error("Fatal Error", e);
+                }
+            }
+            if (null != httpClient) {
+                try {
+                    httpClient.close();
+                } catch (IOException e) {
+                    log.error("Fatal Error", e);
+                }
+            }
         }
-        return "ERROR";
+        return result;
     }
 }
