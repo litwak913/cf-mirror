931dacf6b097584ac013fb24ffa92592cc3aff03
litwak913 <litwak913@qq.com>
2020-07-12 07:08:37 +0800

    add args tests

--- src/main/java/io/github/litwak913/Config.java
+++ src/main/java/io/github/litwak913/Config.java
@@ -5,12 +5,12 @@ import com.beust.jcommander.Parameter;
 
 public class Config {
     @Parameter(names = "--output", description = "Set output Dir", order = 1)
-    String dir = "./output";
+    public String dir = "./output";
     @Parameter(names = "--mode",
             description = "Set mirror mode",
             required = true,
             order = 0)
-    String mode;
+    public String mode;
     @Parameter(names = "--help", help = true, order = 2, description = "display this help and exit")
     boolean help;
     @Parameter(names = "--version", help = true, order = 3, description = "output version information and exit")
