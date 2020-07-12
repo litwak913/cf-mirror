ad159ef764d1637188ee107d8124e102caed950d
litwak913 <litwak913@qq.com>
2020-07-12 11:39:21 +0800

    format data

--- src/main/java/io/github/litwak913/Mods.java
+++ src/main/java/io/github/litwak913/Mods.java
@@ -15,14 +15,13 @@ public class Mods {
         this.slug = slug;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public int getId() {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
